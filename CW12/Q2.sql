29bcba097b303f82952ae6dea0936fed1555574b
HosseinTahami <hosseintahami2000@gmail.com>
2023-06-22 14:47:00 +0330

    CW12, Q2, Done

---
+++ CW12/Q2.sql
@@ -0,0 +1,5 @@
+SELECT title, release_year
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Action' OR category.name = 'Comedy' OR category.name = 'Family'
