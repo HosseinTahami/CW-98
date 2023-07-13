1b1f542a1a39524d01c1f83087223da7869b7144
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-13 16:53:17 +0330

    __str__ added to each class

--- CW15/BlogPlatform/BlogApp/models.py
+++ CW15/BlogPlatform/BlogApp/models.py
@@ -5,18 +5,27 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField()
 
+    def __str__(self) -> str:
+        return f"{self.name} : {self.bio}"
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.CharField(max_length=200)
     date = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f"{self.title} : {self.content} : {self.author} : {self.date}"
+    
 class Comment(models.Model):
     post = models.TextField()
     author = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{self.post} : {self.author} : {self.date}"
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-
+    def __str__(self) -> str:
+        return f"{self.name} : {self.description}"
 
