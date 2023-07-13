f2802ed0241eb7ddb8ea58acf2adedf95fed48cd
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-13 15:12:15 +0330

    add tem to the setting.py

--- CW15/BlogPlatform/BlogPlatform/settings.py
+++ CW15/BlogPlatform/BlogPlatform/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BlogPlatform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'tem'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
