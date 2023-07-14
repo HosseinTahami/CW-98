68b571ae0017149b4572beb08000ad19d0872f50
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 11:09:30 +0330

    nothing

--- CW15/BlogPlatform/BlogApp/models.py
+++ CW15/BlogPlatform/BlogApp/models.py
@@ -12,6 +12,17 @@ class Category(models.Model):
     description = models.TextField()
     def __str__(self) -> str:
         return f"{self.name} : {self.description}"
+
+class Comment(models.Model):
+    post = models.TextField()
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return f"{self.post} : {self.author} : {self.date}"
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -23,19 +34,16 @@ class Post(models.Model):
         Category,
         on_delete=models.CASCADE
     )
+    
+    comment = models.ForeignKey(
+        Comment,
+        on_delete = models.CASCADE
+    )
     date = models.DateField(auto_now_add=True)
     def __str__(self) -> str:
         return f"{self.title} : {self.content} : {self.author} : {self.date}"
 
-class Comment(models.Model):
-    post = models.TextField()
-    author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE
-    )
-    date = models.DateTimeField(auto_now_add=True)
-    def __str__(self) -> str:
-        return f"{self.post} : {self.author} : {self.date}"
+
 
 
 
