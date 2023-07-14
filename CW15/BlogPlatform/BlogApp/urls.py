f9fe337eab2cfc2adfdb75a8a13b1cf8b4f382ce
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 09:45:31 +0330

    working on urls and html

--- CW15/BlogPlatform/BlogApp/urls.py
+++ CW15/BlogPlatform/BlogApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list),
-    path('categories/', views.category_list),
-    path('authors/', views.author_list),
+    path('posts/', views.post_list, name = 'posts'),
+    path('categories/', views.category_list, name = 'categories'),
+    path('authors/', views.author_list, name = 'authors'),
     path('posts/<int:post_id>/', views.post_details),
     path('categories/<int:category_id/', views.category_details),
     path('authors/<int:author_id>/', views.author_details)
