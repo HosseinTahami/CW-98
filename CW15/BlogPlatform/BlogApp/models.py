4dbbf8aed66b1de9714ba530ccfda2a030941deb
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 10:32:51 +0330

    new models created

--- CW15/BlogPlatform/BlogApp/models.py
+++ CW15/BlogPlatform/BlogApp/models.py
@@ -4,28 +4,40 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField()
-
     def __str__(self) -> str:
         return f"{self.name} : {self.bio}"
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )    
     date = models.DateField(auto_now_add=True)
-    
     def __str__(self) -> str:
         return f"{self.title} : {self.content} : {self.author} : {self.date}"
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.PROTECT
+    )
+    def __str__(self) -> str:
+        return f"{self.name} : {self.description}"
+
+
     
 class Comment(models.Model):
     post = models.TextField()
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE
+    )
     date = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
         return f"{self.post} : {self.author} : {self.date}"
 
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    def __str__(self) -> str:
-        return f"{self.name} : {self.description}"
+
 
