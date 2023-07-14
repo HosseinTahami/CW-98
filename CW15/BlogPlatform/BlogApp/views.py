962bd99a6a6c62d743087c397edd37e44677e6ae
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 06:38:16 +0330

    post list created

--- CW15/BlogPlatform/BlogApp/views.py
+++ CW15/BlogPlatform/BlogApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def show_posts(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'all':posts})
+
+def show_categories(request):
+    pass
+
+def show_authors(request):
+    pass
