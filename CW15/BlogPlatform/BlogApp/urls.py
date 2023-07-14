d60f057344633b6307c6549b5af84568385ef47c
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 07:37:49 +0330

    adding urls

--- CW15/BlogPlatform/BlogApp/urls.py
+++ CW15/BlogPlatform/BlogApp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_list) ,
-    path('categories/', views.category_list) ,
-    path('authors/', views.author_list) 
+    path('posts/', views.post_list),
+    path('categories/', views.category_list),
+    path('authors/', views.author_list),
+    path('posts/<int:post_id>/', views.post_details),
+    path('categories/<int:category_id', views.category_details),
+    path('authors/<author_id:int>/', views.author_details)
 ]
