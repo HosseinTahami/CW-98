2058ecc97483ee2871bc5039945fc7f308d21674
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-14 07:22:21 +0330

    post, category, author list

--- CW15/BlogPlatform/BlogApp/urls.py
+++ CW15/BlogPlatform/BlogApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.show_posts) ,
-    path('categories/', views.show_categories) ,
-    path('authors/', views.show_authors) 
+    path('posts/', views.post_list) ,
+    path('categories/', views.category_list) ,
+    path('authors/', views.author_list) 
 ]
