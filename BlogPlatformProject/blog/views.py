7528e9b458e1739e17e6b17e35bc3c9c540170f8
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-18 19:35:50 +0330

    add

---
+++ BlogPlatformProject/blog/views.py
@@ -0,0 +1,40 @@
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category, Comment
+from users.models import Author
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def home(request):
+    if request.method == "POST" :
+        searched = request.POST['searched']
+        result = Post.objects.filter(Q(title__icontains=searched) | Q(content__icontains=searched)).distinct()
+        context = {'results':result}
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'base.html')
+
+def post_list(request):
+    all_posts = Post.objects.all()
+    return render(request, "Blog/post_list.html", {"all_posts": all_posts})
+
+
+def post_details(request, pk):
+    context = {}
+    if request.method == 'GET':
+        post = Post.objects.get(id=pk)
+        context = {"post": post}
+    return render(request, "Blog/post.html", context)
+
+
+def category_list(request):
+    all_category = Category.objects.all()
+    return render(request, "Blog/category_list.html", {"all_category": all_category})
+
+
+def category_details(request, pk):
+    category = Category.objects.get(id=pk)
+    return render(request, "Blog/category_details.html", {"category": category })
