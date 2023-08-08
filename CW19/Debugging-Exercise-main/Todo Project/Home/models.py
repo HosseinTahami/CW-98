9a242ee09cb8349b64557ca1a3b8006cadd432dc
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-08 18:56:57 +0330

    fix: comment the abstract

--- CW19/Debugging-Exercise-main/Todo Project/Home/models.py
+++ CW19/Debugging-Exercise-main/Todo Project/Home/models.py
@@ -13,8 +13,8 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
     def __str__(self):
         return self.title
