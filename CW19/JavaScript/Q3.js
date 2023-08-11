913b4bb6612e91bcbb14a98987fdbc2897b673e4
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-11 16:19:39 +0330

    Q3.js Done

--- CW19/JavaScript/Q3.js
+++ CW19/JavaScript/Q3.js
@@ -1,13 +1,25 @@
 function storeUsername() {
     let username = prompt("Enter your username:");
-    document.cookie = `username=${username}; expires=Thu, 18 Dec 2024 12:00:00 UTC; path=/"`;
-    let usernameCookie = document.cookie;
-    greetUser(usernameCookie);
+    if (!greetUser(document.cookie)) {
+      document.cookie = `username=${username};`
+    }
+    else{
+      if (username === greetUser(document.cookie)){
+        alert('Welcome back')
+      }
+      else{
+        alert(`Welcome ${username}`)
+        document.cookie = `username=${username};`
+      }
+    }
+    //greetUser(document.cookie);
   }
 
 function greetUser(usernameCookie) {
     let splitCookie = usernameCookie.split(';');
-    alert(`Welcome ${splitCookie[0]}`);
+    let finalUsername = splitCookie[0].substring('username='.length);
+    return finalUsername
+    //alert(`Welcome ${finalUsername}`);
 }
 
 storeUsername();
