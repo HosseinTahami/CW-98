6408e242a0f4b9ee982202675f1ce494658595af
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-11 14:51:34 +0330

    Q3.js Done

--- CW19/JavaScript/Q3.js
+++ CW19/JavaScript/Q3.js
@@ -1,6 +1,12 @@
 function storeUsername() {
     let username = prompt("Enter your username:");
-    document.cookie = `username=${username}`;
-    greetUser();
+    document.cookie = username;
+    let usernameCookie = document.cookie;
+    greetUser(usernameCookie);
   }
 
+function greetUser(usernameCookie) {
+    alert(`Welcome ${usernameCookie}`);
+}
+
+storeUsername();
